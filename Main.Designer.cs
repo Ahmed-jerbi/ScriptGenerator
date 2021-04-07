718f49ecedbd199261a30ed61c231f1d46d7c5da
Ahmed Jerbi <jerbi.ahmed@vioso.com>
2021-04-07 17:40:38 +0200

    Added script file generation

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -44,7 +44,7 @@
             this.WaitBox = new System.Windows.Forms.GroupBox();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
-            this.timeText = new System.Windows.Forms.TextBox();
+            this.waitText = new System.Windows.Forms.TextBox();
             this.CopyBox = new System.Windows.Forms.GroupBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
@@ -57,10 +57,28 @@
             this.label10 = new System.Windows.Forms.Label();
             this.saveText = new System.Windows.Forms.TextBox();
             this.generateScript = new System.Windows.Forms.Button();
+            this.RecalibBox = new System.Windows.Forms.GroupBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.InteractBox = new System.Windows.Forms.ComboBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.compoundText = new System.Windows.Forms.TextBox();
+            this.AddVcBox = new System.Windows.Forms.GroupBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.compoundVCText = new System.Windows.Forms.TextBox();
+            this.CCSBox = new System.Windows.Forms.GroupBox();
+            this.label15 = new System.Windows.Forms.Label();
+            this.ccsText = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.compoundCCSText = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.resetButton = new System.Windows.Forms.Button();
             this.LoadBox.SuspendLayout();
             this.WaitBox.SuspendLayout();
             this.CopyBox.SuspendLayout();
             this.saveBox.SuspendLayout();
+            this.RecalibBox.SuspendLayout();
+            this.AddVcBox.SuspendLayout();
+            this.CCSBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // fLabel
@@ -171,20 +189,20 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(158, 45);
+            this.label3.Location = new System.Drawing.Point(157, 48);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.Size = new System.Drawing.Size(33, 13);
             this.label3.TabIndex = 8;
-            this.label3.Text = ".sps";
+            this.label3.Text = "(.sps)";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 22);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.Size = new System.Drawing.Size(28, 13);
             this.label2.TabIndex = 7;
-            this.label2.Text = "File Name";
+            this.label2.Text = "File ";
             // 
             // loadText
             // 
@@ -199,7 +217,7 @@
             // 
             this.WaitBox.Controls.Add(this.label8);
             this.WaitBox.Controls.Add(this.label9);
-            this.WaitBox.Controls.Add(this.timeText);
+            this.WaitBox.Controls.Add(this.waitText);
             this.WaitBox.ForeColor = System.Drawing.SystemColors.Window;
             this.WaitBox.Location = new System.Drawing.Point(49, 593);
             this.WaitBox.Name = "WaitBox";
@@ -223,19 +241,18 @@
             this.label9.AutoSize = true;
             this.label9.Location = new System.Drawing.Point(12, 30);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(25, 13);
+            this.label9.Size = new System.Drawing.Size(31, 13);
             this.label9.TabIndex = 7;
-            this.label9.Text = "zeit";
-            this.label9.Click += new System.EventHandler(this.label9_Click);
+            this.label9.Text = "Time";
             // 
-            // timeText
+            // waitText
             // 
-            this.timeText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
-            this.timeText.ForeColor = System.Drawing.SystemColors.Window;
-            this.timeText.Location = new System.Drawing.Point(15, 46);
-            this.timeText.Name = "timeText";
-            this.timeText.Size = new System.Drawing.Size(56, 22);
-            this.timeText.TabIndex = 0;
+            this.waitText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.waitText.ForeColor = System.Drawing.SystemColors.Window;
+            this.waitText.Location = new System.Drawing.Point(15, 46);
+            this.waitText.Name = "waitText";
+            this.waitText.Size = new System.Drawing.Size(56, 22);
+            this.waitText.TabIndex = 0;
             // 
             // CopyBox
             // 
@@ -257,11 +274,11 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(158, 92);
+            this.label6.Location = new System.Drawing.Point(157, 94);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.Size = new System.Drawing.Size(33, 13);
             this.label6.TabIndex = 11;
-            this.label6.Text = ".sps";
+            this.label6.Text = "(.sps)";
             // 
             // label7
             // 
@@ -284,11 +301,11 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(158, 53);
+            this.label4.Location = new System.Drawing.Point(157, 55);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(27, 13);
+            this.label4.Size = new System.Drawing.Size(33, 13);
             this.label4.TabIndex = 8;
-            this.label4.Text = ".sps";
+            this.label4.Text = "(.sps)";
             // 
             // label5
             // 
@@ -325,20 +342,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(158, 45);
+            this.label1.Location = new System.Drawing.Point(157, 47);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.Size = new System.Drawing.Size(33, 13);
             this.label1.TabIndex = 8;
-            this.label1.Text = ".sps";
+            this.label1.Text = "(.sps)";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
             this.label10.Location = new System.Drawing.Point(12, 22);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(57, 13);
+            this.label10.Size = new System.Drawing.Size(25, 13);
             this.label10.TabIndex = 7;
-            this.label10.Text = "File Name";
+            this.label10.Text = "File";
             // 
             // saveText
             // 
@@ -361,13 +378,178 @@
             this.generateScript.TabIndex = 17;
             this.generateScript.Text = "Generate";
             this.generateScript.UseVisualStyleBackColor = false;
+            this.generateScript.Click += new System.EventHandler(this.generateScript_Click);
+            // 
+            // RecalibBox
+            // 
+            this.RecalibBox.Controls.Add(this.label11);
+            this.RecalibBox.Controls.Add(this.InteractBox);
+            this.RecalibBox.Controls.Add(this.label12);
+            this.RecalibBox.Controls.Add(this.compoundText);
+            this.RecalibBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.RecalibBox.Location = new System.Drawing.Point(327, 463);
+            this.RecalibBox.Name = "RecalibBox";
+            this.RecalibBox.Size = new System.Drawing.Size(229, 107);
+            this.RecalibBox.TabIndex = 16;
+            this.RecalibBox.TabStop = false;
+            this.RecalibBox.Text = "Recalibrate";
+            this.RecalibBox.Visible = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 72);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(91, 13);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "Interaction Level";
+            // 
+            // InteractBox
+            // 
+            this.InteractBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.InteractBox.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.InteractBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.InteractBox.FormattingEnabled = true;
+            this.InteractBox.Items.AddRange(new object[] {
+            "None",
+            "Minimal",
+            "Reduced",
+            "All"});
+            this.InteractBox.Location = new System.Drawing.Point(120, 69);
+            this.InteractBox.Name = "InteractBox";
+            this.InteractBox.Size = new System.Drawing.Size(103, 21);
+            this.InteractBox.TabIndex = 8;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(12, 22);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(97, 13);
+            this.label12.TabIndex = 7;
+            this.label12.Text = "Compound Name";
+            // 
+            // compoundText
+            // 
+            this.compoundText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.compoundText.ForeColor = System.Drawing.SystemColors.Window;
+            this.compoundText.Location = new System.Drawing.Point(15, 41);
+            this.compoundText.Name = "compoundText";
+            this.compoundText.Size = new System.Drawing.Size(206, 22);
+            this.compoundText.TabIndex = 0;
+            // 
+            // AddVcBox
+            // 
+            this.AddVcBox.Controls.Add(this.label14);
+            this.AddVcBox.Controls.Add(this.compoundVCText);
+            this.AddVcBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.AddVcBox.Location = new System.Drawing.Point(327, 586);
+            this.AddVcBox.Name = "AddVcBox";
+            this.AddVcBox.Size = new System.Drawing.Size(229, 75);
+            this.AddVcBox.TabIndex = 17;
+            this.AddVcBox.TabStop = false;
+            this.AddVcBox.Text = "Add VC to display geometry";
+            this.AddVcBox.Visible = false;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(12, 22);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(97, 13);
+            this.label14.TabIndex = 7;
+            this.label14.Text = "Compound Name";
+            // 
+            // compoundVCText
+            // 
+            this.compoundVCText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.compoundVCText.ForeColor = System.Drawing.SystemColors.Window;
+            this.compoundVCText.Location = new System.Drawing.Point(15, 42);
+            this.compoundVCText.Name = "compoundVCText";
+            this.compoundVCText.Size = new System.Drawing.Size(206, 22);
+            this.compoundVCText.TabIndex = 0;
+            // 
+            // CCSBox
+            // 
+            this.CCSBox.Controls.Add(this.label15);
+            this.CCSBox.Controls.Add(this.ccsText);
+            this.CCSBox.Controls.Add(this.label13);
+            this.CCSBox.Controls.Add(this.compoundCCSText);
+            this.CCSBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.CCSBox.Location = new System.Drawing.Point(327, 689);
+            this.CCSBox.Name = "CCSBox";
+            this.CCSBox.Size = new System.Drawing.Size(229, 125);
+            this.CCSBox.TabIndex = 18;
+            this.CCSBox.TabStop = false;
+            this.CCSBox.Text = "Custom Content Space";
+            this.CCSBox.Visible = false;
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(10, 70);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(82, 13);
+            this.label15.TabIndex = 9;
+            this.label15.Text = "Content Space";
+            // 
+            // ccsText
+            // 
+            this.ccsText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.ccsText.ForeColor = System.Drawing.SystemColors.Window;
+            this.ccsText.Location = new System.Drawing.Point(13, 90);
+            this.ccsText.Name = "ccsText";
+            this.ccsText.Size = new System.Drawing.Size(206, 22);
+            this.ccsText.TabIndex = 8;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(12, 26);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(97, 13);
+            this.label13.TabIndex = 7;
+            this.label13.Text = "Compound Name";
+            // 
+            // compoundCCSText
+            // 
+            this.compoundCCSText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.compoundCCSText.ForeColor = System.Drawing.SystemColors.Window;
+            this.compoundCCSText.Location = new System.Drawing.Point(13, 45);
+            this.compoundCCSText.Name = "compoundCCSText";
+            this.compoundCCSText.Size = new System.Drawing.Size(206, 22);
+            this.compoundCCSText.TabIndex = 0;
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "ini";
+            this.saveFileDialog1.FileName = "MyScript";
+            this.saveFileDialog1.Filter = "VIOSO script |*.ini";
+            // 
+            // resetButton
+            // 
+            this.resetButton.BackColor = System.Drawing.Color.Transparent;
+            this.resetButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.resetButton.Font = new System.Drawing.Font("Segoe UI Black", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.ForeColor = System.Drawing.Color.LightSteelBlue;
+            this.resetButton.Location = new System.Drawing.Point(52, 396);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(54, 44);
+            this.resetButton.TabIndex = 19;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = false;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(43)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
-            this.ClientSize = new System.Drawing.Size(1155, 902);
+            this.ClientSize = new System.Drawing.Size(1155, 460);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.CCSBox);
+            this.Controls.Add(this.AddVcBox);
+            this.Controls.Add(this.RecalibBox);
             this.Controls.Add(this.generateScript);
             this.Controls.Add(this.saveBox);
             this.Controls.Add(this.LoadBox);
@@ -394,6 +576,12 @@
             this.CopyBox.PerformLayout();
             this.saveBox.ResumeLayout(false);
             this.saveBox.PerformLayout();
+            this.RecalibBox.ResumeLayout(false);
+            this.RecalibBox.PerformLayout();
+            this.AddVcBox.ResumeLayout(false);
+            this.AddVcBox.PerformLayout();
+            this.CCSBox.ResumeLayout(false);
+            this.CCSBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -415,7 +603,7 @@
         private System.Windows.Forms.GroupBox WaitBox;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox timeText;
+        private System.Windows.Forms.TextBox waitText;
         public System.Windows.Forms.GroupBox CopyBox;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
@@ -428,6 +616,21 @@
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.TextBox saveText;
         private System.Windows.Forms.Button generateScript;
+        private System.Windows.Forms.GroupBox RecalibBox;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox compoundText;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox InteractBox;
+        private System.Windows.Forms.GroupBox AddVcBox;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox compoundVCText;
+        private System.Windows.Forms.GroupBox CCSBox;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox ccsText;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox compoundCCSText;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button resetButton;
     }
 }
 
--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace JungleDiamond.Properties {
+namespace VIOSOScriptGenerator.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace JungleDiamond.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("JungleDiamond.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VIOSOScriptGenerator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
